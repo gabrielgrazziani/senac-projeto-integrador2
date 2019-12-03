f5cff505ccc575fdb1b47a1766ba65280f15d9a2
gabriel_grazziani <gabriel.grazzianibarbosa@gmail.com>
2019-12-03 14:58:21 -0300

    criando objeto evasao

---
+++ ProjetoIntegrador/src/tratamentoDeDados/Evasao.java
@@ -0,0 +1,42 @@
+package tratamentoDeDados;
+
+import java.util.Calendar;
+
+public class Evasao {
+	private int id;
+	private Calendar data;
+	private String motivo;
+	private String curso;
+	private String area;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Calendar getData() {
+		return data;
+	}
+	public void setData(Calendar data) {
+		this.data = data;
+	}
+	public String getMotivo() {
+		return motivo;
+	}
+	public void setMotivo(String motivo) {
+		this.motivo = motivo;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area;
+	}
+	
+}
