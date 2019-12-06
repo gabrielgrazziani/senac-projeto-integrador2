312654f4147d41ef1758bde2e98e86dc8b54ca01
gabriel_grazziani <gabriel.grazzianibarbosa@gmail.com>
2019-12-06 14:41:12 -0300

    separando em metodos

--- ProjetoIntegrador/src/tela/principal.java
+++ ProjetoIntegrador/src/tela/principal.java
@@ -2,11 +2,16 @@ package tela;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import org.knowm.xchart.CategoryChart;
+import org.knowm.xchart.XChartPanel;
 import com.toedter.calendar.JDateChooser;
+
+import tratamentoDeDados.DadosFiltrados;
+import tratamentoDeDados.Evasao;
+
 import javax.swing.JButton;
 import java.awt.GridBagConstraints;
 import javax.swing.JLabel;
@@ -14,19 +19,27 @@ import javax.swing.JComboBox;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import java.awt.GridLayout;
+import java.util.ArrayList;
 import java.awt.GridBagLayout;
 import com.jgoodies.forms.layout.FormLayout;
 import com.jgoodies.forms.layout.ColumnSpec;
 import com.jgoodies.forms.layout.RowSpec;
 import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
+import javax.swing.JTextArea;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JRadioButtonMenuItem;
 
 public class principal extends JFrame {
-
+	
+	private CategoryChart grafico;
 	private JPanel contentPane;
-	JComboBox comboBoxCurso = new JComboBox();
-	JDateChooser caledarioComeco = new JDateChooser();
-	JDateChooser caledarioFim = new JDateChooser();
+	private JComboBox comboBoxCurso = new JComboBox();
+	private JDateChooser caledarioComeco = new JDateChooser();
+	private JDateChooser caledarioFim = new JDateChooser();
+	private XChartPanel<CategoryChart> painelGrafico;
+	private JTextArea textLegenda = new JTextArea();
+	private DadosFiltrados dados;
 
 	/**
 	 * Launch the application.
@@ -43,14 +56,78 @@ public class principal extends JFrame {
 			}
 		});
 	}
+	
+	private void montarGrafico() {
+		ArrayList<String> nomes = new ArrayList<String>();
+		ArrayList<Integer> numeros = new ArrayList<Integer>();
+		
+//		nomes.add("Outros Motivos");             
+//		nomes.add("Dificuldades Financeiras");      
+//		nomes.add("Não se identificou com o curso");
+//		nomes.add("Não se adaptou a metodologia");  
+//		nomes.add("Motivo de Trabalho");            
+//		nomes.add("Motivo de Estudo");            
+//		nomes.add("Motivo de Saúde");          
+//		nomes.add("Motivo de Viagens");            
+//		nomes.add("Mudança de Município");          
+//		nomes.add("Cancelamento da Programação");  
+//		nomes.add("Adiamento da Programação");  
+//		nomes.add("Incompatibilidade de horário");
+//		nomes.add("Distância / meio de transporte");
+//		nomes.add("Cancelamento de matr¿cula");
+//		nomes.add("Transferência de Programação");
+//		nomes.add("Abandono sem Justificativa");
+//		nomes.add("Alteração de Horário");  
+		nomes.add("Sem dados"); 
+		
+		
+		ArrayList<String> legenda = legendaArrayList(nomes);
+		 
+		for (int i = 0; i < nomes.size(); i++) {
+			numeros.add(i);
+		}
+		
+		arumarLegenda(nomes, legenda);
+		grafico = new CategoryChart(30, 30);
+		grafico.addSeries("Motivos Evasão", legenda, numeros);
+	}
+	
+	private void atualisarGrafico(DadosFiltrados dados) {
+		ArrayList<String> nomes = dados.getMotivoEvasao();
+		ArrayList<Integer> numeros = dados.getFrequenciaEvasao();
+		ArrayList<String> legenda = legendaArrayList(nomes);
+		arumarLegenda(nomes, legenda);
+		
+		grafico.updateCategorySeries("Motivos Evasão", nomes, numeros, null);
+		painelGrafico.repaint();
+	}
+	
+	private ArrayList<String> legendaArrayList(ArrayList<String> nomes){
+		ArrayList<String> legenda = new ArrayList<String>();
+		for (int i = 0; i < nomes.size(); i++) {
+			legenda.add("Motivo " + (i+1));
+		}
+		return legenda;
+	}
+	
+	private void arumarLegenda(ArrayList<String> nomes,ArrayList<String> legenda) {
+		String le = "";
+		for (int i = 0; i < nomes.size(); i++) {
+			le += legenda.get(i) + " - " + nomes.get(i) + "\n";
+		}
+		textLegenda.setText(le);
+	}
+	
+	private void arumarTextRelatorio(DadosFiltrados dados) {
+		//textArea.setText("teste teste\nteste");
+	}
 
 	/**
 	 * Create the frame.
 	 */
 	public principal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		
+		setExtendedState(JFrame.MAXIMIZED_BOTH);
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 		
@@ -69,38 +146,50 @@ public class principal extends JFrame {
 		setContentPane(contentPane);
 		JPanel panelMenu = new JPanel();
 		contentPane.add(panelMenu, BorderLayout.WEST);
+		panelMenu.setLayout(new GridLayout(2, 1, 0, 0));
+		GridBagConstraints gbc_lblDataComeo = new GridBagConstraints();
 		
-		JLabel lblCurso = new JLabel("Curso");
-		panelMenu.setLayout(new GridLayout(10, 1, 0, 0));
-		panelMenu.add(lblCurso);
+		JPanel panelOp = new JPanel();
+		panelMenu.add(panelOp);
+		panelOp.setLayout(new GridLayout(12, 1, 0, 0));
 		
-		
-		panelMenu.add(comboBoxCurso);
+		JLabel lblCurso = new JLabel("Curso                          ");
+		panelOp.add(lblCurso);
+		panelOp.add(comboBoxCurso);
 		
 		JLabel lblDataComeo = new JLabel("Data come\u00E7o");
-		GridBagConstraints gbc_lblDataComeo = new GridBagConstraints();
-		panelMenu.add(lblDataComeo);
-		
+		panelOp.add(lblDataComeo);
+		panelOp.add(caledarioComeco);
 		
 		caledarioComeco.setDateFormatString("dd/MM/yy");
-		panelMenu.add(caledarioComeco);
 		
 		JLabel lblDataFim = new JLabel("Data fim");
-		panelMenu.add(lblDataFim);
-		
+		panelOp.add(lblDataFim);
+		panelOp.add(caledarioFim);
 		
 		caledarioFim.setDateFormatString("dd/MM/yy");
-		panelMenu.add(caledarioFim);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		panelMenu.add(scrollPane_1);
+		
+		textLegenda.setEditable(false);
+		scrollPane_1.setViewportView(textLegenda);
 		
 		JPanel panelDados = new JPanel();
 		contentPane.add(panelDados, BorderLayout.CENTER);
 		panelDados.setLayout(new GridLayout(2, 1, 0, 0));
 		
+		montarGrafico();
+		painelGrafico = new XChartPanel<CategoryChart>(grafico);
+		
+		//panelDados.add(painelGrafico);
+		
 		JScrollPane scrollPane = new JScrollPane();
 		panelDados.add(scrollPane);
 		
-		JTextPane textPane = new JTextPane();
-		scrollPane.setViewportView(textPane);
+		JTextArea textArea = new JTextArea();
+		textArea.setEditable(false);
+		scrollPane.setViewportView(textArea);
 	}
 
 }
